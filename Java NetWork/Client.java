07ac19fb90e823cbd22235067f46ff033d8115b3
imagebutton2 <1046509851@qq.com>
2019-01-03 23:07:03 +0800

    利用UDP协议实现一个简单的白板程序。 用户在客户端界面上单击鼠标左键后程序在鼠标单击位置画20*20的矩形，单击右键后程序在鼠标单击位置画20*20的圆。服务器界面上将同时显示与客户端一致的画面。

---
+++ Java NetWork/Client.java
@@ -0,0 +1,160 @@
+﻿import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+import javax.swing.JFrame;
+
+
+public class Client extends JFrame { 
+
+	private static final long serialVersionUID = 1L;
+	Graphics g;//定义了一个画笔
+	private Integer x;//坐标x
+	private Integer y;//坐标y
+	private String flag;//标志flag判断是圆还是正方形
+	private DatagramSocket socket=null;//定义了一个Socket套接字
+	
+	private String flag1;//标志flag1判断是何种颜色
+	private int color1;
+	private int color2;
+	private int color3;
+	private String	[] string=new String [3];
+	private Color color;
+
+/**
+ * 以上为服务 画笔 颜色的变量
+ */
+
+	public Client(){
+		
+		this.setTitle("Client");
+		this.setSize(800, 800);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		this.addMouseListener();//添加鼠标事件监听
+
+		
+		int i=0;
+		string[0]="灰色";
+		string[1]="蓝色";
+		string[2]="红色";
+        i=(int)(Math.random()*3);
+        System.out.println(i);
+        System.out.println(string[i]);
+		if("红色".equals(string[i])) {
+			color1=255;
+			color2=0;
+			color3=0;
+		}
+		
+		if("蓝色".equals(string[i])) {
+			color1=0;
+			color2=0;
+			color3=255;
+		}
+		
+		if("灰色".equals(string[i])) {
+			color1=128;
+			color2=128;
+			color3=128;
+		}
+		
+        flag1=string[i];
+        System.out.println(flag1);
+        color=new Color(color1, color2, color3,255);
+
+ /*
+ 以上为调制画笔颜色
+ */
+        
+	}
+	
+	public void addMouseListener() {
+//鼠标事件响应程序
+		addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent me){
+				if(me.getButton()==MouseEvent.BUTTON1){//判断是否为鼠标左键
+					x=me.getX();
+					y=me.getY();
+					flag="Rectangle";//形状为正方形
+					print();//画图函数
+					sendUdp();
+				}
+				else if(me.getButton()==MouseEvent.BUTTON3){//判断是否为鼠标右键
+					g=getGraphics();
+					x=me.getX();
+					y=me.getY();
+					flag="Circle";//形状为圆
+					print();
+					sendUdp();
+					
+				}
+			}
+		});
+		
+	}
+	
+	//画图函数
+	public void print(){
+		g=getGraphics();
+		g.setColor(color);
+		if("Rectangle".equals(flag)){	//判断形状是否为正方形		
+			g.drawRect(x, y, 20, 20);//画正方形 长20 宽20
+		}
+		else if("Circle".equals(flag)){//判断形状是否为圆	
+			g.drawOval(x, y, 40, 40);//画圆 直径40
+		}
+	}
+	
+	public void sendUdp() {
+		try {			
+			socket = new DatagramSocket();//创建一个数据报
+			byte[] Send1=(""+x).getBytes();//将坐标x转化为byte数组
+			byte[] Send2=(""+y).getBytes();//将坐标y转化为byte数组
+			byte[] Send3=flag.getBytes();//将flag转化为byte数组
+			byte[] Send4=flag1.getBytes();//将flag1转化为byte数组
+			DatagramPacket pack1=new DatagramPacket(Send1, 0, Send1.length, 
+					InetAddress.getByName("127.0.0.1"), 9090);		
+			DatagramPacket pack2=new DatagramPacket(Send2, 0, Send2.length, 
+					InetAddress.getByName("127.0.0.1"), 9090);
+			DatagramPacket pack3=new DatagramPacket(Send3, 0, Send3.length, 
+					InetAddress.getByName("127.0.0.1"), 9090);
+			DatagramPacket pack4=new DatagramPacket(Send4, 0, Send4.length, 
+					InetAddress.getByName("127.0.0.1"), 9090);
+			//将坐标x，坐标y，flag，flag1封装成数据报；  ip地址为本地回环地址  端口号9090
+			socket.send(pack1);
+			socket.send(pack2);
+			socket.send(pack3);
+			socket.send(pack4);
+			//将数据报发送给服务器端
+			System.out.println(x);
+			System.out.println(y);
+			System.out.println(flag);
+			
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}finally {
+		if(socket!=null)
+		{
+			socket.close();
+		}
+	}
+	}
+			
+	public static void main(String[] args){
+		//事件分派线程
+		EventQueue.invokeLater(() ->{
+			Client client=new Client();
+		});
+	}		
+	
+}
---
+++ Java NetWork/Serve.java
@@ -0,0 +1,120 @@
+﻿import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+import javax.swing.JFrame;
+
+public class Serve extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private DatagramSocket socket;//定义了一个Socket套接字
+	private Graphics g;//画笔
+	
+	private Integer x;//坐标x
+	private Integer y;//坐标y
+	private String flag;//标志flag判断是圆还是正方形
+	
+	private String flag1;//标志flag1判断是何种颜色
+	private int color1;
+	private int color2;
+	private int color3;
+	private String	[] string=new String [3];
+	private Color color;
+/**
+以上为服务 画笔 颜色的变量
+ */
+public Serve(){
+	string[0]="灰色";
+	string[1]="蓝色";
+	string[2]="红色";
+}
+
+	public void udpReceive() {
+
+		// 接收客户端发来的数据（核心）
+		try {
+			socket = new DatagramSocket(9090);//创建一个数据报端口号为9090
+			while (true) {
+				byte[] b1 = new byte[100];
+				byte[] b2 = new byte[100];
+				byte[] b3 = new byte[100];
+				byte[] b4 = new byte[100];
+				
+				DatagramPacket packet1 = new DatagramPacket(b1, b1.length);
+				DatagramPacket packet2 = new DatagramPacket(b2, b2.length);
+				DatagramPacket packet3 = new DatagramPacket(b3, b3.length);
+				DatagramPacket packet4 = new DatagramPacket(b4, b4.length);
+				socket.receive(packet1);
+				socket.receive(packet2);
+				socket.receive(packet3);
+				socket.receive(packet4);
+				
+				flag  = new String(packet3.getData(), 0, packet3.getLength());
+				flag1  = new String(packet4.getData(), 0, packet4.getLength());
+				x = Integer.parseInt(new String(packet1.getData(), 0, packet1.getLength()));
+				y = Integer.parseInt(new String(packet2.getData(), 0, packet2.getLength()));
+				 System.out.println(x);
+				 System.out.println(y);
+				 System.out.println(flag);
+				 System.out.println(flag1);
+				 
+				 if("红色".equals(flag1)) {
+						color1=255;
+						color2=0;
+						color3=0;
+					}
+				
+				 if("蓝色".equals(flag1)) {
+						color1=0;
+						color2=0;
+						color3=255;
+					}
+				
+				 if("灰色".equals(flag1)) {
+						color1=128;
+						color2=128;
+						color3=128;
+					}
+				color=new Color(color1, color2, color3,255);
+				
+				print(); //画图函数
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			if(socket!=null) {
+				socket.close();
+			}
+			
+		}
+	}
+	
+	//画图函数
+	public void print(){
+		g=getGraphics();
+		g.setColor(color);
+		if("Rectangle".equals(flag)){	//判断形状是否为正方形		
+			g.drawRect(x, y, 20, 20);//画正方形 长20 宽20
+		}
+		else if("Circle".equals(flag)){//判断形状是否为圆	
+			g.drawOval(x, y, 40, 40);//画圆 直径40
+		}
+	}
+	
+	public static void main(String[] args) throws SocketException {
+		// TODO Auto-generated method stub
+		//事件分派线程
+		EventQueue.invokeLater(() ->{
+
+		Serve serve=new Serve();
+		serve.setTitle("Serve");
+		serve.setSize(800, 800);
+		serve.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		serve.setVisible(true);		
+		serve.udpReceive();
+		});
+	}	
+		
+}
